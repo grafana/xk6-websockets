89d1b52c4359dc55e0d3a6cc295c1256e1ac9b7e
Joan López de la Franca Beltran <joanjan14@gmail.com>
2024-06-20 19:45:52 +0200

    Add some tests for Blob's public methods

---
+++ websockets/blob_test.go
@@ -0,0 +1,49 @@
+package websockets
+
+import (
+	"testing"
+
+	"github.com/grafana/sobek"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBlob_text(t *testing.T) {
+	ts := newTestState(t)
+	val, err := ts.runtime.RunOnEventLoop(`
+		const blob = new Blob(["P", "A", "SS"]);
+		blob.text();
+	`)
+	require.NoError(t, err)
+	require.Equal(t, "PASS", val.String())
+}
+
+func TestBlob_arrayBuffer(t *testing.T) {
+	ts := newTestState(t)
+	val, err := ts.runtime.RunOnEventLoop(`
+		const blob = new Blob(["P", "A", "SS"]);
+		blob.arrayBuffer();
+	`)
+	require.NoError(t, err)
+
+	ab, ok := val.Export().(sobek.ArrayBuffer)
+	require.True(t, ok)
+	require.Equal(t, "PASS", string(ab.Bytes()))
+}
+
+func TestBlob_stream(t *testing.T) {
+	ts := newTestState(t)
+	val, err := ts.runtime.RunOnEventLoop(`
+		(async () => {
+          const blob = new Blob(["P", "A", "SS"]);
+		  const reader = blob.stream().getReader();
+		  const {value} = await reader.read();
+		  return value;
+		})()
+	`)
+	require.NoError(t, err)
+
+	p, ok := val.Export().(*sobek.Promise)
+	require.True(t, ok)
+	assert.Equal(t, "PASS", p.Result().String())
+}
